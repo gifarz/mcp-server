741860db5a2c35a17db692805df54087e65aadf7
Gifar Zaini <gifarz06@gmail.com>
2026-09-11 17:57:43 +0700

    adjustment on ecosystem js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
     apps: [{
         name: 'wyntrax-mcp',
         script: 'src/index.http.js',
-        cwd: '/var/www/wyntrax-mcp',
-        env_file: '/var/www/wyntrax/.env',  // point to wyntrax's .env
+        cwd: '/var/www/wyntrax/product/mcp-server',
+        env: { HTTP_PORT: 3011 },
+        env_file: '/var/www/wyntrax/product/mcp-server/.env',
     }]
 }
